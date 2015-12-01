cbca8091e7b1bb94580a1ac142575953b0b6f873
Gregory Soloschenko <soloschenko@gmail.com>
2015-12-01 14:05:04 -0600

    test ci

--- Gruntfile.js
+++ Gruntfile.js
@@ -351,7 +351,7 @@ module.exports = function (grunt) {
         'clean:server',
         'createDefaultTemplate',
         'handlebars',
-        'compass',
+        // 'compass',
         'connect:testserver',
         'exec:mocha'
     ]);
